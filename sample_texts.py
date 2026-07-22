21c572bbfb85005a17bc5a4b55bad707aa59e26f
elizarpirog <elizarpir@ukr.net>
2026-07-22 22:15:59 +0300

    Add sample_texts.py with demo TextDocument

---
+++ sample_texts.py
@@ -0,0 +1,25 @@
+from models import TextDocument
+
+TEXTS = [
+
+    TextDocument(
+
+        "demo_book.txt",
+
+        """
+        Madam Anna went to level village.
+
+        There she met Hannah.
+
+        Someone wrote racecar on the wall.
+
+        The mechanic repaired a rotator and a rotavator.
+
+        Refer to civic duties every day.
+
+        Radar systems detected another level signal.
+        """
+
+    )
+
+]
